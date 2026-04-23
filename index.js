d975bcce4ea863f2ff77ca8f02ba5ee7ff903c9d
phelisianjackson934-source <phelisianjackson934@gmail.com>
2026-04-23 20:33:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-const express = require('express');
-const app = express();
-app.use(express.urlencoded({ extended: false }));
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
 
-app.post('/whatsapp', (req, res) => {
-  const msg = req.body.Body;
-  res.type('text/xml').send(`<Response><Message>Mambo! Nimepokea: ${msg}. Festo Bot iko Live 🔥</Message></Response>`);
+const client = new Client({
+    authStrategy: new LocalAuth(),
+    puppeteer: { 
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox'] 
+    }
 });
 
-app.get('/', (req, res) => res.send('Festo Bot iko Live'));
-app.listen(process.env.PORT || 3000);
+client.on('qr', qr => {
+    console.log('FESTO BOT: SCAN QR HII');
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('FESTO BOT IKO LIVE SASA!');
+});
+
+client.initialize();
+
+// Server ya Render isilale
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Festo Bot Running'));
+app.listen(process.env.PORT || 10000, () => console.log('Server ready'));
