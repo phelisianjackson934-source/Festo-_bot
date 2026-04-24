93ab952b4de081f787386e3a0789cf9f2cddda41
phelisianjackson934-source <phelisianjackson934@gmail.com>
2026-04-24 17:03:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,9 @@ sock.ev.on('connection.update', async (update) => {
     const { connection, lastDisconnect, qr } = update;
     
     if(qr && !sock.authState.creds.registered) {
-        console.log("====== TUMIA HII PAIRING CODE ======");
+        console.log("====== TUMIA PAIRING CODE ======");
         const code = await sock.requestPairingCode("255687702991");
         console.log("CODE YAKO: ", code);
-        console.log("====================================");
+        console.log("=================================");
+        console.log("Nenda WhatsApp > Linked Devices > Link with phone number");
     }
-    
-    if(connection === 'close') {
-        // ... code zingine
-    } else if(connection === 'open') {
-        console.log('BOT IMEUNGANISHWA NA WHATSAPP! ✅');
-    }
-});
